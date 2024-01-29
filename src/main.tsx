ceed3dba8d86525c4556bddfc7ea7dcc6ebe3c41
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-01-29 15:21:55 +0700

    config: setup react router, chakra ui, and jest

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { ChakraProvider } from '@chakra-ui/react';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './routes/index.tsx';
+import { theme } from './theme/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={routes} />
+    </ChakraProvider>
+  </React.StrictMode>
+);
