b2178f794f62e2e1c70b64fa74ae556d84779465
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-01-30 21:36:40 +0700

    feat(index): add more testcases

--- src/pages/index.spec.tsx
+++ src/pages/index.spec.tsx
@@ -1,4 +1,5 @@
 // renders without crashing
+import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import Index from './index';
 
@@ -6,5 +7,17 @@ describe('Index', () => {
   it('renders without crashing', () => {
     render(<Index />);
     expect(true).toBeTruthy();
+  });
+  it('renders heading', () => {
+    const { getByText } = render(<Index />);
+    const heading = getByText(/Recipes Book/i);
+    expect(heading).toBeInTheDocument();
+  });
+  it('renders search input', () => {
+    const { getByPlaceholderText } = render(<Index />);
+    const searchInput = getByPlaceholderText(
+      /Search recipe name\/ingredients/i
+    );
+    expect(searchInput).toBeInTheDocument();
   });
 });
