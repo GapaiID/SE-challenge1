c85ba8fd54733ccd3b47bfc331c1e26e1ea3df94
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-01-31 07:04:29 +0700

    misc: change recipes list

--- src/constants/recipes.ts
+++ src/constants/recipes.ts
@@ -104,7 +104,57 @@ export const RECIPES: Recipe[] = [
   {
     id: '3',
     name: 'Sandwich Mozarella 3',
-    category: 'Breakfast',
+    category: 'Lunch',
+    image:
+      'https://www.thecandidcooks.com/wp-content/uploads/2022/08/prosciutto-mozzarella-sandwich-horiz2.jpg',
+    ingredients: [
+      {
+        name: 'Roti tawar',
+        amount: 2,
+        unit: 'slice',
+      },
+      {
+        name: 'Keju Mozarella',
+        amount: 2,
+        unit: 'slice',
+      },
+      {
+        name: 'Tomat',
+        amount: 1,
+        unit: 'slice',
+      },
+      {
+        name: 'Selada',
+        amount: 1,
+        unit: 'slice',
+      },
+      {
+        name: 'Mayonaise',
+        amount: 1,
+        unit: 'sendok makan',
+      },
+      {
+        name: 'Mentega',
+        amount: 1,
+        unit: 'sendok makan',
+      },
+    ],
+    steps: [
+      'Oleskan mentega pada roti tawar',
+      'Letakkan keju mozarella, tomat, dan selada',
+      'Tambahkan mayonaise',
+      'Tutup dengan roti tawar',
+    ],
+    cookingTime: {
+      amount: 10,
+      unit: 'menit',
+    },
+    servings: 1,
+  },
+  {
+    id: '4',
+    name: 'Sandwich Mozarella 4',
+    category: 'Dessert',
     image:
       'https://www.thecandidcooks.com/wp-content/uploads/2022/08/prosciutto-mozzarella-sandwich-horiz2.jpg',
     ingredients: [
