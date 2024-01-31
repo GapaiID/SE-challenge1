d47ffaa8e9bdeb53be8b526a2402cfc1bc27b63f
Alifia Rahmah <alifiarahmah03@gmail.com>
2024-01-31 08:16:01 +0700

    fix: add memoryrouter to tests

--- src/pages/detail.spec.tsx
+++ src/pages/detail.spec.tsx
@@ -1,11 +1,25 @@
 // renders without crashing
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Detail from './detail';
 
 describe('Detail', () => {
   it('renders without crashing', () => {
-    render(<Detail />);
+    render(
+      <MemoryRouter>
+        <Detail />
+      </MemoryRouter>
+    );
     expect(true).toBeTruthy();
+  });
+  it('renders heading', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/detail/unknown']}>
+        <Detail />
+      </MemoryRouter>
+    );
+    const heading = getByText(/not found/i);
+    expect(heading).toBeInTheDocument();
   });
 });
--- src/pages/index.spec.tsx
+++ src/pages/index.spec.tsx
@@ -1,20 +1,33 @@
 // renders without crashing
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Index from './index';
 
 describe('Index', () => {
   it('renders without crashing', () => {
-    render(<Index />);
+    render(
+      <MemoryRouter>
+        <Index />
+      </MemoryRouter>
+    );
     expect(true).toBeTruthy();
   });
   it('renders heading', () => {
-    const { getByText } = render(<Index />);
+    const { getByText } = render(
+      <MemoryRouter>
+        <Index />
+      </MemoryRouter>
+    );
     const heading = getByText(/Recipes Book/i);
     expect(heading).toBeInTheDocument();
   });
   it('renders search input', () => {
-    const { getByPlaceholderText } = render(<Index />);
+    const { getByPlaceholderText } = render(
+      <MemoryRouter>
+        <Index />
+      </MemoryRouter>
+    );
     const searchInput = getByPlaceholderText(
       /Search recipe name\/ingredients/i
     );
